0f8cfdf77be142a4d3e66693a877469c69ece300
matthias-stumm <eurobos112@gmail.com>
2026-04-22 20:50:09 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("meinButton").addEventListener("click", function () {
+  document.getElementById("ausgabe").textContent = "Hallo Matthias, Deine JavaScript-Funktion läuft.";
+});
